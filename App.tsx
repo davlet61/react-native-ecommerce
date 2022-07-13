9b27eb8e34fefc8e57f024a180b8fac1d50e3b57
Dovlat <dovlat93@gmail.com>
2022-07-13 23:16:51 +0200

    renames components to screens

--- App.tsx
+++ App.tsx
@@ -3,8 +3,8 @@ import { StyleSheet, Text, View } from 'react-native';
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useFonts } from 'expo-font';
-import Home from './components/Home';
-import Details from './components/Details';
+import Home from './screens/Home';
+import Details from './screens/Details';
 
 const Stack = createNativeStackNavigator();
 
